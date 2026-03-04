b85572a721d35e81038f0869d42806fdd83f769f
cainydev <wajo432@gmail.com>
2026-03-04 16:51:13 +0100

    Change environment rule for when Umami Script is loaded

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,17 +1,20 @@
-<meta charset="utf-8"/>
-<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta charset="utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-<link rel="manifest" href="/favicon/site.webmanifest">
+<link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+<link rel="manifest" href="/favicon/site.webmanifest" />
 
-<link rel="preconnect" href="https://fonts.bunny.net">
-<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet"/>
+<link rel="preconnect" href="https://fonts.bunny.net" />
+<link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@vite(["resources/css/app.css", "resources/js/app.js"])
 @fluxAppearance
 
-@production
-    <script defer src="https://{{ config('services.umami.domain') }}/script.js" data-website-id="{{ config('services.umami.website_id') }}"></script>
-@endproduction
+@env(["prod", "production"])
+    <script
+        defer
+        src="https://{{ config("services.umami.domain") }}/script.js"
+        data-website-id="{{ config("services.umami.website_id") }}"></script>
+@endenv
